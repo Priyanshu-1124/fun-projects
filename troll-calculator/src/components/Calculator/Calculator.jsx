55f4a56066c1f89b21bf82d5e3384a04982e705d
Priyanshu-1124 <priyanshusom007@gmail.com>
2025-10-18 19:26:39 +0530

    feat(calculator): make the buttons functional

--- troll-calculator/src/components/Calculator/Calculator.jsx
+++ troll-calculator/src/components/Calculator/Calculator.jsx
@@ -1,50 +1,54 @@
+import { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+
+  const [output, setOutput] = useState("")
+
   return (
     <div className="container">
 
       <div className="output-area">
-        <p className="result"></p>
+        <p className="result">{output}</p>
       </div>
 
       <div className="button-grid">
         <div>
-          <button>C</button>
-          <button>bksp</button>
-          <button>(</button>
-          <button>)</button>
+          <button onClick={()=>setOutput("")}>C</button>
+          <button onClick={()=>setOutput(output.slice(0, output.length-1))}>bksp</button>
+          <button onClick={()=>setOutput(output+'(')}>(</button>
+          <button onClick={()=>setOutput(output+')')}>)</button>
         </div>
 
         <div>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>÷</button>
+          <button onClick={()=>setOutput(output+'7')}>7</button>
+          <button onClick={()=>setOutput(output+'8')}>8</button>
+          <button onClick={()=>setOutput(output+'9')}>9</button>
+          <button onClick={()=>setOutput(output+'/')}>/</button>
         </div>
 
         <div>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>x</button>
+          <button onClick={()=>setOutput(output+'4')}>4</button>
+          <button onClick={()=>setOutput(output+'5')}>5</button>
+          <button onClick={()=>setOutput(output+'6')}>6</button>
+          <button onClick={()=>setOutput(output+'x')}>x</button>
         </div>
 
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>-</button>
+          <button onClick={()=>setOutput(output+'1')}>1</button>
+          <button onClick={()=>setOutput(output+'2')}>2</button>
+          <button onClick={()=>setOutput(output+'3')}>3</button>
+          <button onClick={()=>setOutput(output+'-')}>-</button>
         </div>
 
         <div>
-          <button>0</button>
-          <button>.</button>
+          <button onClick={()=>setOutput(output+'0')}>0</button>
+          <button onClick={()=>setOutput(output+'.')}>.</button>
           <button>=</button>
-          <button>+</button>
+          <button onClick={()=>setOutput(output+'+')}>+</button>
         </div>
       </div>
-      
+
     </div>
   );
 };
